6074d8689f94f85a250ff942ffabd3dae81997d7
Ruan Linos Alves <ruandeveloper@hotmail.com>
2020-04-16 15:42:17 -0300

    Finalized challenge with all tests passed.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+import { response } from 'express';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,49 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.filter(item => item.type === 'income');
+    const outcome = this.transactions.filter(item => item.type === 'outcome');
+
+    const totalIncome = income.reduce(
+      (accumulator, transaction) => {
+        const sum = accumulator.value + transaction.value;
+
+        return {
+          value: sum,
+        };
+      },
+      { value: 0 }, // Initial Value <--
+    );
+
+    const totalOutcome = outcome.reduce(
+      (accumulator, transaction) => {
+        const sum = accumulator.value + transaction.value;
+
+        return {
+          value: sum,
+        };
+      },
+      { value: 0 }, // Initial Value <--
+    );
+    const total = totalIncome.value - totalOutcome.value;
+    return { income: totalIncome.value, outcome: totalOutcome.value, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    const balance = this.getBalance();
+
+    if (type === 'outcome' && balance.total < value) {
+      throw Error('you dont have enough balance');
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
